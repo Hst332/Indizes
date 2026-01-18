ba1a5d05d00caa21abc264bbb9b9e80ce7ef186b
Hst332 <hst332@gmail.com>
2026-01-18 21:05:39 +0100

    Create asset_config.py

---
+++ src/assets/asset_config.py
@@ -0,0 +1,32 @@
+ASSETS = {
+    "DAX": {
+        "ticker": "^GDAXI",
+        "timezone": "Europe/Berlin",
+        "market": "EU"
+    },
+    "ATX": {
+        "ticker": "^ATX",
+        "timezone": "Europe/Vienna",
+        "market": "EU"
+    },
+    "DOW": {
+        "ticker": "^DJI",
+        "timezone": "US/Eastern",
+        "market": "US"
+    },
+    "NASDAQ": {
+        "ticker": "^IXIC",
+        "timezone": "US/Eastern",
+        "market": "US"
+    },
+    "SP500": {
+        "ticker": "^GSPC",
+        "timezone": "US/Eastern",
+        "market": "US"
+    },
+    "NIKKEI": {
+        "ticker": "^N225",
+        "timezone": "Asia/Tokyo",
+        "market": "JP"
+    }
+}
