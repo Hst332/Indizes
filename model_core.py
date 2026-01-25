7b3fa6dbe61f56e897c81866b54743cbaa94a63f
Hst332 <hst332@gmail.com>
2026-01-25 20:34:18 +0100

    Update model_core.py

--- model_core.py
+++ model_core.py
@@ -1,10 +1,7 @@
-# model_core.py
-
-def run_model(market_data):
+def run_model(df):
     """
-    Platzhalter-Modell.
+    Platzhalter-Modell
     """
     return {
-        "signal": "HOLD",
-        "confidence": 0.0
+        "score": 0.0
     }
