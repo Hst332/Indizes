801b8295a4e76c6e4a058ba01af514ede4f33ceb
Hst332 <hst332@gmail.com>
2026-01-27 20:59:51 +0100

    Update decision_engine.py

--- decision_engine.py
+++ decision_engine.py
@@ -4,12 +4,12 @@ def generate_signal(model_output, regime):
     """
     score = model_output.get("score", 0.0)
 
-if score > 0.002:
-    signal = "BUY"
-elif score < -0.002:
-    signal = "SELL"
-else:
-    signal = "HOLD"
+    if score > 0.002:
+        signal = "BUY"
+    elif score < -0.002:
+        signal = "SELL"
+    else:
+        signal = "HOLD"
 
     return {
         "signal": signal,
