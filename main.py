5a7bc2fecc45470078dfc223b2a0a69af66a29b4
Hst332 <hst332@gmail.com>
2026-01-27 07:58:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from forecast_runner import run_all_forecasts
 from forecast_writer import write_forecasts
 
-
 if __name__ == "__main__":
     forecasts = run_all_forecasts()
     write_forecasts(forecasts)
