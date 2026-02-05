fa348d7ef792019f4d3a51bafb6fb5a0f32f143a
Hst332 <hst332@gmail.com>
2026-02-05 12:46:57 +0100

    Remove unused import statement from asset_config.py

--- asset_config.py
+++ asset_config.py
@@ -1,6 +1,6 @@
 from backtest_engine import run_backtest
 from backtest_writer import summarize_backtest, save_backtest_csv
-from assets_config import ASSETS   # <--- ggf. ändern auf deine Datei!
+
 
 
 if __name__ == "__main__":
